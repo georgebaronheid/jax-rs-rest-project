c783b829a0ee1b314547f33d310e1308af60d812
George Baronheid <baronheid.george@gmail.com>
2020-04-22 21:32:28 -0300

    Entity manager factory

--- src/main/java/br/com/baronheid/singleton/EntityManagerSingletonFactory.java
+++ src/main/java/br/com/baronheid/singleton/EntityManagerSingletonFactory.java
@@ -1,4 +1,20 @@
 package main.java.br.com.baronheid.singleton;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class EntityManagerSingletonFactory {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    // We do not intend to allow this object to be created
+    private EntityManagerSingletonFactory(){}
+
+    // This static method generates an entity manager to supply persistence to the application
+    public static EntityManagerFactory getInstance() {
+        if (entityManagerFactory == null) {
+            entityManagerFactory = Persistence.createEntityManagerFactory("ORACLE_UNIT");
+        }
+        return entityManagerFactory;
+    }
 }
